7d9c74f42e6d011f2404a723c97accd476ede657
Hamas Akif Sanie <hamasakifsanie@gmail.com>
2024-04-01 16:13:05 +0700

    commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let pilihanPlayer = prompt("masukkan pilihan batu kertas gunting")
+let pilihanComputer;
+
+function computer() {
+  const pilihan = ["batu", "kertas", "gunting"]
+  let random = Math.floor(Math.random() * pilihan.length)
+  return pilihan[random]
+}
+
+pilihanComputer = computer()
+
+if (pilihanComputer === pilihanPlayer) {
+  console.log("sama")
+}
+// if (pilihanComputer === "batu" && pilihanPlayer === "batu") {
+//   console.log("tie")
+// } else if (pilihanComputer === "kertas" && pilihanPlayer === "kertas") {
+//   console.log("tie")
+// } else if (pilihanComputer === "gunting" && pilihanPlayer === "gunting") {
+//   console.log("tie")
+// } else if ()
