1c7030984a6ec6268835c543ef57955e2f8954ad
Hamas Akif Sanie <hamasakifsanie@gmail.com>
2024-04-12 00:52:34 +0700

    finished UI

--- js/script.js
+++ js/script.js
@@ -1,22 +1,42 @@
-// let pilihanPlayer = prompt("masukkan pilihan batu kertas gunting")
-// let pilihanComputer;
+const winLose = document.querySelector('.winLose')
+const rps = document.querySelector('.rps')
+const fistLeft = document.querySelector('#fist-left')
+const fistRight = document.querySelector('#fist-right')
+const playerScore = document.querySelector('#player-score')
+const computerScore = document.querySelector('#computer-score')
+const rockButton = document.querySelector('#rockButton')
+const paperButton = document.querySelector('#paperButton')
+const scissorButton = document.querySelector('#scissorButton')
+const restartButton = document.querySelector('.restart-button')
+const startButtons = document.querySelector('#startButtons')
+const startButton = document.querySelector('#startButton')
 
-// function computer() {
-//   const pilihan = ["batu", "kertas", "gunting"]
-//   let random = Math.floor(Math.random() * pilihan.length)
-//   return pilihan[random]
-// }
+// make start button disappear
+startButton.addEventListener("click",() => {
+    startButtons.classList.add('display-none')
+})
 
-// pilihanComputer = computer()
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-// if (pilihanComputer === pilihanPlayer) {
-//   console.log("sama")
-// }
-// // if (pilihanComputer === "batu" && pilihanPlayer === "batu") {
-// //   console.log("tie")
-// // } else if (pilihanComputer === "kertas" && pilihanPlayer === "kertas") {
-// //   console.log("tie")
-// // } else if (pilihanComputer === "gunting" && pilihanPlayer === "gunting") {
-// //   console.log("tie")
-// // } else if ()
+document.getElementById('startButton').addEventListener('click', async function() {
+    this.classList.add('hidden');
+    await sleep(2000)
+    this.classList.add('disappear') // Add 'hidden' class to the button
+    });
+
+// make game start and blink
+startButton.addEventListener('click', async function () {
+    await sleep(2000)
+    winLose.textContent = "Game Start"
+    rps.textContent = "Choose Your Weapon"
+    rps.classList.add("fade-in-out")
+    winLose.classList.add("blinkGameStart")
+})
+
+//restart button refresh
+restartButton.addEventListener("click",() => {
+    location.reload()
+})
 
